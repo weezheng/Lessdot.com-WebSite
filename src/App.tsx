5c61e467b2a078a6dd441ba4fba3aef99969b6dc
ZhengWee <enwee.vip@gmail.com>
2026-04-19 23:06:02 +0800

    update to hash router for subpage display

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import ProfessionalIndex from "./pages/ProfessionalIndex";
 import GeekIndex from "./pages/GeekIndex";
