7d61acddf71c23af76625040d31794e325ff1dc4
ZhengWee <enwee.vip@gmail.com>
2026-04-19 22:41:31 +0800

    NA

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/', // 如果是自定义域名，设为 '/'
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
